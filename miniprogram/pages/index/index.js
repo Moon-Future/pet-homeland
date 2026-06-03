1d618076876c95f79fd928d85a5b228a69f96e99
Leon <236338364@qq.com>
2026-06-03 09:27:06 +0800

    feat: build home page mockup

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,5 +1,32 @@
 Page({
   data: {
-    appName: "Pet Homeland",
+    homeBg: "https://qiniu.cdn.cl8023.com/project/star-paws/images/home-bg.png",
+    recentMemorial: {
+      petName: "奶球",
+      relationDays: 2116,
+      message: "今天有 12 位朋友来看它",
+      avatar: "/assets/home/default-pet.svg",
+      supporters: ["晴", "安", "夏", "米"],
+    },
+    navItems: [
+      { label: "首页", icon: "/assets/icons/sparkle.svg", active: true },
+      { label: "纪念馆", icon: "/assets/icons/memorial.svg" },
+      { label: "发现", icon: "/assets/icons/star.svg" },
+      { label: "我的", icon: "/assets/icons/paw.svg" },
+    ],
+  },
+
+  onCreateMemorial() {
+    wx.showToast({
+      title: "创建流程开发中",
+      icon: "none",
+    });
+  },
+
+  onViewAll() {
+    wx.showToast({
+      title: "纪念馆列表开发中",
+      icon: "none",
+    });
   },
 });
